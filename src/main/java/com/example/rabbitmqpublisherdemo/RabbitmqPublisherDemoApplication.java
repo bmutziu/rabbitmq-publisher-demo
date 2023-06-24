07820bbdd19d3809059ecb10bf2b899cf04c7ea2
Radit Panjapiyakul <radit.pan@ascendcorp.com>
2023-06-24 11:30:24 +0700

    Initialize project

---
+++ src/main/java/com/example/rabbitmqpublisherdemo/RabbitmqPublisherDemoApplication.java
@@ -0,0 +1,13 @@
+package com.example.rabbitmqpublisherdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RabbitmqPublisherDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RabbitmqPublisherDemoApplication.class, args);
+	}
+
+}
